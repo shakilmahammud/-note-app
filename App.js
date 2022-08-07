744b0c9fb773d14db796da421246aec816fc1472
shakilmahammud <hshakil987@gmail.com>
2022-08-07 22:58:41 +0600

    update

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/home'
+import SignIn from './src/screens/signin'
+import Signup from './src/screens/signup'
+import Edit from './src/screens/edit'
+import Create from './src/screens/create'
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+     <Stack.Screen name="Home" component={Home} />
+     <Stack.Screen name="Signin" component={SignIn} />
+     <Stack.Screen name="Signup" component={Signup} />
+     <Stack.Screen name="Create" component={Create} />
+     <Stack.Screen name="Edit" component={Edit} />
+     </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
---
+++ src/screens/signup.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <View>
+      <Text>signup</Text>
+    </View>
+  )
+}
